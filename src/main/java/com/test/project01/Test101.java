4c943c7d94bd992ed47bac326b82577e009c2cad
bball11 <bball11@naver.com>
2015-11-16 09:51:30 +0900

    new branch02

--- src/main/java/com/test/project01/Test101.java
+++ src/main/java/com/test/project01/Test101.java
@@ -1,7 +1,18 @@
 package com.test.project01;
 
 public class Test101 {
+	
+	private String message = null;
+	
 	public Test101() {
-		// 01
+		this.message = "";
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
 	}
 }
